bf382303574af8b5bc2d433cda38f921bfbbc907
jackylimel <veryfaint@gmail.com>
2019-01-11 11:41:31 +1100

    refactoring: simplify provider methods

--- xtzmaster/Data/Provider/AccountProvider.swift
+++ xtzmaster/Data/Provider/AccountProvider.swift
@@ -6,65 +6,68 @@ struct AccountProvider {
   let provider = MoyaProvider<XTZMasterAPI>()
   
   func getAccount(address: String) -> Observable<Account> {
-    return Observable.just(address)
-      .flatMap { address in
-        self.provider.rx.request(.getAccount(accountAddress: address))
-      }
-      .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
-      .flatMap { (response) -> Observable<Account> in
-        return self.mapResponseTo(response: response)
-      }
-      .catchError({ (error) in
-        return Observable.error(error)
-      })
+    let request = provider.rx.request(.getAccount(accountAddress: address))
+    let mapping = { response -> Observable<Account> in
+      self.mapResponseTo(response: response)
+    }
+    return executeRequest(request: request, mapping: mapping)
   }
   
   func getStaking(address: String) -> Observable<String> {
-    return Observable.just(address)
-      .flatMap { address in
-        self.provider.rx.request(.getStakingBalance(accountAddress: address))
-      }
-      .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
-      .flatMap { (response) -> Observable<String> in
+    let request = provider.rx.request(.getStakingBalance(accountAddress: address))
+    let mapping = { (response: Response) -> Observable<String> in
+      do {
         if let json = try response.mapJSON() as? [String] {
-          return Observable.just(json.first!)
+          return Observable.just(json.first ?? "0")
         }
         return Observable.just("0")
+      } catch {
+        return Observable.just("0")
       }
-      .catchError({ (error) in
-        return Observable.error(error)
-      })
+    }
+    
+    return executeRequest(request: request, mapping: mapping)
   }
   
   func getAllBakedAddress(bakerAddress: String) -> Observable<[String]> {
-    return Observable.just(bakerAddress)
-      .flatMap { address in
-        self.provider.rx.request(.getAllBakedAddresses(bakerAddress: address))
-      }
-      .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
-      .flatMap { (response) -> Observable<[String]> in
+    let request = provider.rx.request(.getAllBakedAddresses(bakerAddress: bakerAddress))
+    let mapping = { (response: Response) -> Observable<[String]> in
+      do {
         if let json = try response.mapJSON() as? [String] {
           return Observable.just(json)
         }
         return Observable.just([])
+      } catch {
+        return Observable.just([])
       }
-      .catchError({ (error) in
-        return Observable.error(error)
-      })
+    }
+    return executeRequest(request: request, mapping: mapping)
   }
   
   func getCurrentCycle() -> Observable<Int> {
-    return Observable.just(true)
-      .flatMap { _ in
-        MoyaProvider<XTZMasterAPI>().rx.request(.getHead())
-      }
-      .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
-      .flatMap { (response) -> Observable<Int> in
+    let request = provider.rx.request(.getHead())
+    let mapping = { (response: Response) -> Observable<Int> in
+      do {
         if let json = try response.mapJSON() as? [String: Any],
-           let level = json["level"] as? Int {
+          let level = json["level"] as? Int {
           return Observable.just(level / 4096)
         }
         return Observable.just(0)
+      } catch {
+        return Observable.just(0)
+      }
+    }
+    return executeRequest(request: request, mapping: mapping)
+  }
+  
+  private func executeRequest<T>(request: PrimitiveSequence<SingleTrait, Response>, mapping: @escaping (Response) -> Observable<T> ) -> Observable<T> where T: Codable {
+    return Observable.just(true)
+      .flatMap { _ in
+        request
+      }
+      .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
+      .flatMap { (response) -> Observable<T> in
+        return mapping(response)
       }
       .catchError({ (error) in
         return Observable.error(error)
