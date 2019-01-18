18f8922bde714defbe4470a0c1b37c1a3e1e23b8
jackylimel <veryfaint@gmail.com>
2019-01-18 20:25:18 +1100

    add cycle to Account

--- xtzmaster/Data/DataModel/Account.swift
+++ xtzmaster/Data/DataModel/Account.swift
@@ -5,11 +5,13 @@ struct Account: Codable {
   let manager: AccountName
   let balance: Double
   let staking: Double
+  let cycle: Int
   
   enum CodingKeys: String, CodingKey {
     case name
     case manager
     case balance
+    case cycle
     case staking = "staking_balance"
   }
 }
