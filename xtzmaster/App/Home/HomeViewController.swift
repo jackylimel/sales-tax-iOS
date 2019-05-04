addfdcb61bf49a824b11f6c026d7ba37065f7af2
jackylimel <veryfaint@gmail.com>
2019-05-05 09:52:59 +1000

    change TabBarItem tintcolor, add something to home

--- xtzmaster/App/Home/HomeViewController.swift
+++ xtzmaster/App/Home/HomeViewController.swift
@@ -6,13 +6,18 @@ class HomeViewController: UIViewController {
   private var disposeBag: DisposeBag = DisposeBag()
   let usecase = GetAccountInfoUseCase()
   
+  @IBOutlet weak var delegationButton: UIButton!
+
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     self.tabBarItem = UITabBarItem(title: "Home", image: R.image.home(), selectedImage: R.image.homeSelected())
+    self.navigationItem.title = "XTZMaster"
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    delegationButton.layer.cornerRadius = 5
     
     usecase.getRootAccount(by: "tz1KfEsrtDaA1sX7vdM4qmEPWuSytuqCDp5j")
       .subscribe(onNext: { account in
